50fd21ddd25a0c6078db6ff51e968563f9c5a47f
robiulalam76 <robiulalam762400@gmail.com>
2023-03-29 17:24:16 +0600

    footer bg images updated

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -4,13 +4,14 @@ import twitter from "../../../assets/icons/twitter.png"
 import instagram from "../../../assets/icons/instagram.png"
 import whatsup from "../../../assets/icons/whatsup.png"
 import img1 from '../../../assets/images/banner-images/img1.png'
+import banner from '../../../assets/images/footer-images/img1.png'
 
 const Footer = () => {
     return (
         <section className='max-w-[1440px] mx-auto px-4 mt-32 pb-6'>
-            <div className='w-full h-[700px] rounded-t-3xl relative' >
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7496150.225879024!2d85.84603134112554!3d23.452181092347267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30adaaed80e18ba7%3A0xf2d28e0c4e1fc6b!2sBangladesh!5e0!3m2!1sen!2sbd!4v1680084534328!5m2!1sen!2sbd" className='w-full h-full rounded-t-3xl' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                <div className='w-full h-[700px] absolute top-0 z-10 bg-secondary bg-opacity-75'></div>
+            <div style={{ backgroundImage: `url(${banner})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}
+                className='w-full h-[700px] rounded-t-3xl relative' >
+
                 <div className='flex flex-col justify-center items-center w-full h-[700px] absolute top-0 z-50 p-4 md:p-10'>
                     <div className='grid md:grid-cols-2 gap-4 w-full'>
                         <div className='flex flex-col items-center md:items-end gap-4 w-full h-full'>
