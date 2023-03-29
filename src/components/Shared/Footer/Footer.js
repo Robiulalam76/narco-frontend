79fc2fe765af701cf10e1ed4bc41e8ca1e46c6e8
robiulalam76 <robiulalam762400@gmail.com>
2023-03-29 17:32:25 +0600

    footer position updated

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -10,9 +10,9 @@ const Footer = () => {
     return (
         <section className='max-w-[1440px] mx-auto px-4 mt-32 pb-6'>
             <div style={{ backgroundImage: `url(${banner})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}
-                className='w-full h-[700px] rounded-t-3xl relative' >
+                className='w-full h-[700px] rounded-t-[50px] relative overflow-hidden' >
 
-                <div className='flex flex-col justify-center items-center w-full h-[700px] absolute top-0 z-50 p-4 md:p-10'>
+                <div className='flex flex-col justify-center items-center w-full h-[700px] absolute top-0 z-50 p-4 md:p-10 mt-8'>
                     <div className='grid md:grid-cols-2 gap-4 w-full'>
                         <div className='flex flex-col items-center md:items-end gap-4 w-full h-full'>
                             <button className='w-44 h-16 mx-auto md:mr-0 flex justify-around items-center bg-white hover:bg-gray-200 rounded-2xl'>
@@ -35,7 +35,7 @@ const Footer = () => {
 
                         <div className='order-first md:order-none flex flex-col items-center md:items-end gap-6 w-full h-full'>
                             <h1 className='text-5xl font-bold text-white text-center md:text-right'>تواصل معنّا</h1>
-                            <div className='flex flex-col items-center md:items-end gap-2 w-full h-full text-white'>
+                            <div className='flex flex-col items-center md:items-end gap-2 w-full h-full text-center md:text-right text-white'>
                                 <span>hello@narco.com: الإيميل</span>
                                 <span>A23, Ave 14 street, Saudi Arabia Riyadh 23456: العنوان</span>
                                 <span>الجوال: 0558854275</span>
@@ -43,7 +43,7 @@ const Footer = () => {
                         </div>
                     </div>
 
-                    <div className='pt-44 w-full'>
+                    <div className='pt-32 w-full'>
                         <h1 className='text-center font-bold text-3xl md:text-5xl text-white'>شركة ناركو للموارد البشرية</h1>
                         <img className='w-24 mx-auto mr-0' src={img1} alt="" />
                     </div>
