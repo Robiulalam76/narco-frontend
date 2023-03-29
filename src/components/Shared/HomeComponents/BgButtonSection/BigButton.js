6d972dd1f3ab1d8c45f90140de98781688efc231
robiulalam76 <robiulalam762400@gmail.com>
2023-03-29 15:00:09 +0600

    big button section complete

---
+++ src/components/Shared/HomeComponents/BgButtonSection/BigButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import eye from "../../../../assets/icons/big-button-icons/eye.png"
+import leftArrow from "../../../../assets/icons/big-button-icons/leftArrow.png"
+import pdf from '../../../../utils/pdf/narco.pdf'
+
+const BigButton = () => {
+    return (
+        <Link target="_blank" to={pdf} className='h-96 md:h-[600px] w-full grid md:grid-cols-2 items-center gap-4 p-10 md:p-20 rounded-3xl mt-40 bg-primary hover:bg-darkPrimary duration-500 cursor-pointer'>
+            <div className='w-full h-full flex justify-center items-center'>
+                <img className='w-40 md:w-80' src={eye} alt="" />
+            </div>
+            <div className='flex flex-col justify-end items-end gap-8 w-full h-full'>
+                <div className='flex items-end gap-4'>
+                    <img className='w-20' src={leftArrow} alt="" />
+                    <h1 className='text-white text-2xl font-bold'>الملف التعريفي</h1>
+                </div>
+                <h1 className='text-white font-bold text-5xl'>تعّرف علينا أكثر</h1>
+            </div>
+        </Link>
+    );
+};
+
+export default BigButton;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from '../../components/Shared/HomeComponents/AboutSection/About';
 import Banner from '../../components/Shared/HomeComponents/Banner/Banner';
+import BigButton from '../../components/Shared/HomeComponents/BgButtonSection/BigButton';
 import Companies from '../../components/Shared/HomeComponents/CompaniesSection/Companies';
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <div className='max-w-[1440px] mx-auto px-4'>
                 <About />
                 <Companies />
+                <BigButton />
             </div>
         </main>
     );
